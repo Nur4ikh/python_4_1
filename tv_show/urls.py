5099bbab1b13fc5fdf75d325e52381ece27b3fa1
Nur4ikh <136956457+Nur4ikh@users.noreply.github.com>
2023-08-23 22:58:57 +0600

    use CRUD for films😍👌

--- tv_show/urls.py
+++ tv_show/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('films/', views.films_view, name='films'),
-    path('films/<int:id>/', views.films_detail_view, name='films_detail')
+    path('films/<int:id>/', views.films_detail_view, name='films_detail'),
+    path('films/<int:id>/delete/', views.delete_films, name='films_delete'),
+    path('films/<int:id>/update/', views.update_films, name='films_update'),
+    path('add-films/', views.create_films, name='add_films'),
 ]
--- tv_show/views.py
+++ tv_show/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
 
 def films_view(request):
     films = models.TvShow.objects.all()
@@ -8,3 +9,38 @@ def films_view(request):
 def films_detail_view(request, id):
     films_id = get_object_or_404(models.TvShow, id=id)
     return render(request, 'films/films_detail.html', {'films_id': films_id})
+
+
+def create_films(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.FilmsForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Succesfully!')
+    else:
+        form = forms.FilmsForm()
+
+    return render(request, 'films/crud/add_films.html', {'form': form})
+
+def delete_films(request, id):
+    films_id = get_object_or_404(models.TvShow, id=id)
+    films_id.delete()
+    return HttpResponse('Succesfully deleted!')
+
+
+def update_films(request, id):
+    films_id = get_object_or_404(models.TvShow, id=id)
+    if request.method == 'POST':
+        form = forms.FilmsForm(instance=films_id, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Succesfully add!')
+    else:
+        form = forms.FilmsForm(instance=films_id)
+
+    context = {
+        'form': form,
+        'films_id': films_id
+    }
+    return render(request, 'films/crud/update_films.html', context)
